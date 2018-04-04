bc2e1a0dc54a647d2b6c6b5991f22a82fef1befd
Azamat Bekmuratov and Randy Weyer <randy@>
2018-04-04 10:45:47 -0700

    Add logic for playing with one die.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,34 @@
 // Business Logic
 var playerOneScore = 0;
 var playerTwoScore = 0;
+var roundTotal = 0;
 
 function getDiceOutput() {
   var diceResult = Math.floor(Math.random() * 6) + 1;
   return diceResult;
 }
 
-function collectScores(currentScore, totalScore) {
-  totalScore += currentScore;
-  return totalScore;
+function collectScoresForTurn(currentScore, roundTotal) {
+  roundTotal += currentScore;
+  return roundTotal;
+}
+
+function changeTurnsPlayerOne() {
+  playerOneScore += roundTotal;
+    roundTotal = 0;
+  $(".player-one-buttons").toggle();
+  $(".player-two-buttons").toggle();
+  $(".player-one-number").text(playerOneScore);
+  $(".player-one-round-total").text(roundTotal);
+}
+
+function changeTurnsPlayerTwo() {
+  playerTwoScore += roundTotal;
+    roundTotal = 0;
+  $(".player-one-buttons").toggle();
+  $(".player-two-buttons").toggle();
+  $(".player-two-number").text(playerTwoScore);
+  $(".player-two-round-total").text(roundTotal);
 }
 
 // Front End Logic
@@ -18,31 +37,49 @@ $(document).ready(function(){
   $("#player-one-roll").click(function(event) {
     event.preventDefault();
     var rollOutput = getDiceOutput();
+    console.log(rollOutput);
 
-    playerOneScore = collectScores(rollOutput,playerOneScore);
-    console.log(playerOneScore);
-    $(".player-one-number").text(playerOneScore);
+    if (rollOutput===1) {
+      roundTotal = 0;
+      changeTurnsPlayerOne();
+      console.log(roundTotal);
+    } else {
+    roundTotal = collectScoresForTurn(rollOutput,roundTotal);
+    console.log(roundTotal);
+    $(".player-one-round-total").text(roundTotal);
+    }
+
   });
 
   $("#player-one-hold").click(function(event)  {
     event.preventDefault();
-    $(".player-one-buttons").toggle();
-    $(".player-two-buttons").toggle();
+    changeTurnsPlayerOne();
+    if (playerOneScore>=20){
+      alert("player One win!!!");
+    }
+    console.log(playerOneScore);
   });
 //player two
   $("#player-two-roll").click(function(event) {
     event.preventDefault();
     var rollOutput = getDiceOutput();
+    console.log(rollOutput);
+    if (rollOutput===1) {
+      roundTotal = 0;
+      changeTurnsPlayerTwo();
+    } else {
+      roundTotal = collectScoresForTurn(rollOutput,roundTotal);
+      $(".player-two-round-total").text(roundTotal);
+    }
 
-    playerTwoScore = collectScores(rollOutput,playerTwoScore);
-    console.log(playerTwoScore);
-    $(".player-two-number").text(playerTwoScore);
   });
 
   $("#player-two-hold").click(function(event)  {
     event.preventDefault();
-    $(".player-two-buttons").toggle();
-    $(".player-one-buttons").toggle();
+    changeTurnsPlayerTwo();
+    if (playerTwoScore>=20){
+      alert("player Two win!!!");
+    }
   });
 
 });
